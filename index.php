adb78f12f634382dc92ccd97be2ff3ff306134b9
William Mandai <wm@bootstrapstore.net>
2014-09-08 15:04:21 +0300

    Removed timezone

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-date_default_timezone_set('Africa/Nairobi');
+//date_default_timezone_set('Africa/Nairobi');
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
